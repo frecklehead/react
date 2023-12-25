709962f7b69af672928a0821570d907cfa3d0693
frecklehead <prashamsa35np@gmail.com>
2023-12-25 22:33:30 +0545

    1

--- create-react-comp/src/App.jsx
+++ create-react-comp/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import KgButton from "./KgButton";
+function App(){
+  return <div>
+     <h1>
+    I am learn React . 2024 is my year and i am gonna be lucky as hell this year.
+    </h1>
+     <KgButton></KgButton>
 
-function App() {
-  const [count, setCount] = useState(0)
+   </div>
+ 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+}//functional component
 
-export default App
+export default App;//to use app component somewhere else
+
---
+++ create-react-comp/src/KgButton.jsx
@@ -0,0 +1,4 @@
+function KgButton(){
+  return <button>Luck activate</button>
+}
+export default KgButton;
