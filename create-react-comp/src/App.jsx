accdf4f96919001ee59320db05873b8d68b33b1b
frecklehead <prashamsa35np@gmail.com>
2023-12-28 21:52:37 +0545

    bootstrap

--- create-react-comp/src/App.jsx
+++ create-react-comp/src/App.jsx
@@ -1,10 +1,17 @@
 import KgButton from "./KgButton";
+import Hello from "./hello";
+import Random from "./random";
 function App(){
   return <div>
      <h1>
     I am learn React . 2024 is my year and i am gonna be lucky as hell this year.
     </h1>
      <KgButton></KgButton>
+     <Hello> </Hello>
+     <Random></Random>
+     <Random></Random>
+     <Random></Random>
+     <Random />
 
    </div>
  
---
+++ create-react-comp/src/hello.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+  function fullname() {
+    return " Prarary Aryal"
+  }
+  return <h3>
+    hello this is {fullname()}.
+  </h3>
+}
+export default Hello;
