eaf684b7a47e4abb9898e92638c53bc95968ba10
frecklehead <prashamsa35np@gmail.com>
2024-01-07 23:22:43 +0545

    1

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -1,15 +1,21 @@
 import Appname from "/home/frecklehead/Desktop/react/Projects/src/components/appname.jsx";
 import Addtodo from "./components/apptodo";
 import Todoitem from "./components/todoitem";
+import "/home/frecklehead/Desktop/react/Projects/src/app.css"
+
 function App() {
   return (
+    <>
+    
+    
     <center class=" todo-container">
-      <Appname/>
-      <div class="container text-center ">
-        <Addtodo></Addtodo>
+    <Appname/>
+  <Addtodo></Addtodo>
         <Todoitem></Todoitem>
-      </div>
+     
     </center>
+    </>
+   
   );
 }
 
